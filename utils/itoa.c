11bec1c3fa51a21875f449fed6a067fd1a7d8612
Eric LING <eriling@e1r8p12.42.fr>
2021-01-16 09:24:17 +0100

    norme itoa

--- utils/itoa.c
+++ utils/itoa.c
@@ -6,11 +6,27 @@
 /*   By: eriling <eriling@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/08 08:20:20 by eriling           #+#    #+#             */
-/*   Updated: 2021/01/11 15:20:12 by eriling          ###   ########.fr       */
+/*   Updated: 2021/01/16 09:23:41 by eriling          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/printf.h"
+
+void itoa_var_init(unsigned int *num, int *nbr, int *i, int *sign)
+{
+	if (*nbr < 0)
+		*num = -(*nbr);
+	else
+		*num = *nbr;
+	if (*nbr < 0)
+		*i = 2;
+	else
+		*i = 1;
+	if (*nbr < 0)
+		*sign = -1;
+	else 	
+		*sign = 0;
+}
 
 char	*ft_itoa(int nbr)
 {
@@ -19,10 +35,7 @@ char	*ft_itoa(int nbr)
 	int	sign;
 	int	i;
 	
-	num = (nbr < 0) ? -nbr : nbr;
-	i = (nbr < 0) ? 2 : 1;
-	sign = (nbr < 0) ? -1 : 0;
-
+	itoa_var_init(&num, &nbr, &i, &sign);
 	while((num /= 10) > 0)
 		i++;
 	if(!(str =(char*)malloc(sizeof(*str)*i+1)))
@@ -35,6 +48,7 @@ char	*ft_itoa(int nbr)
 		str[i] = ((num % 10) + '0'); 
 		num /= 10;
 	}
-	sign == -1 ? str[0] = '-' : 0;
+	if (sign == -1)
+		str[0] = '-';
 	return(str);
 }
