2f0b40ac4248a26d3fac9604137ebe33c376556b
Eric LING <eriling@e1r7p14.42.fr>
2021-01-18 15:54:32 +0100

    same problem

--- print_hex/print_hex.c
+++ print_hex/print_hex.c
@@ -6,7 +6,7 @@
 /*   By: eriling <eriling@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/18 09:36:21 by eriling           #+#    #+#             */
-/*   Updated: 2021/01/18 14:50:53 by eriling          ###   ########.fr       */
+/*   Updated: 2021/01/18 15:43:48 by eriling          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,44 +29,27 @@ void print_hex(t_param *param, va_list arg, t_param_len *len_printf)
 	char maj;
 
 	var_init_maj(param, &maj);
-	res = ft_itoa_base((unsigned int)va_arg(arg, unsigned int),16, maj);
+	res = ft_itoa_base((unsigned int)va_arg(arg, int),16, maj);
 	len = ft_strlen(res);
 	var_init_hex(param, &space, &zero_precision, len);
+	space = 0;
+	zero_precision = 0;
 	//if (res[0] == '-' && (param->point >= len || (param->width > len && param->zero == 1)))
 		//res_is_neg(zero_precision, space, res, len_printf, param);
 	//else if (param->point > 0)
 	
-
-(void)len_printf;
+	//printf("width:%d\n",param->width);
 	if (param->point > 0)
 	{
-	 //precision_is_positive(zero_precision, space, res, len_printf, param);
+	 precision_is_positive(zero_precision, space, res, len_printf, param);
 	}	
 	else if (param->point == 0 && ft_strcmp("0",res) == 0)
 	{
-		//res_is_blank(space,len_printf, param);
+		res_is_blank(space,len_printf, param);
 	}
 	else
 	{
-		if (param->minus == 1)
-		{
-			ft_putstr(res,len_printf);
-			print_space(space, ' ',len_printf);
-		}
-		else if (param->zero == 1 && param->point != 0)
-		{
-			print_space(space,'0',len_printf);
-			ft_putstr(res,len_printf);
-		}
-		else
-		{
-			printf("space:%d\n",space);
-			print_struct(*param);
-			printf("\n-------------------\n");
-			//print_space(space, ' ',len_printf);
-			//ft_putstr(res,len_printf);
-		}
-		
+		is_else(space, res, len_printf, param);
 	}
 
 		
--- print_hex/var_init_hex.c
+++ print_hex/var_init_hex.c
@@ -6,7 +6,7 @@
 /*   By: eriling <eriling@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/18 14:14:53 by eriling           #+#    #+#             */
-/*   Updated: 2021/01/18 14:15:56 by eriling          ###   ########.fr       */
+/*   Updated: 2021/01/18 15:32:27 by eriling          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,9 @@ void var_init_hex(t_param *param, int *space, int *zero_precision, int len)
 	if (len > param->width)
 		*space = 0;
 	else
-		*space = param->width - len;
+		*space = param->width + len;
 	if (len > param->point)
 		*zero_precision = 0;
 	else
-		*zero_precision = param->point - len;
+		*zero_precision = param->point + len;
 }
