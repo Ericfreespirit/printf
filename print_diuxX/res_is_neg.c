885fa2ddf7655ead1e992c00a0d8035831af8e2d
Eric LING <eriling@e1r7p14.42.fr>
2021-01-20 12:35:00 +0100

    513/513 test done

--- print_diuxX/res_is_neg.c
+++ print_diuxX/res_is_neg.c
@@ -6,7 +6,7 @@
 /*   By: eriling <eriling@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/15 15:25:28 by eriling           #+#    #+#             */
-/*   Updated: 2021/01/20 11:57:04 by eriling          ###   ########.fr       */
+/*   Updated: 2021/01/20 12:27:10 by eriling          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,15 +37,6 @@ void res_is_neg(int zero_precision, int space, char *res, t_param_len *len_print
 		print_space(zero_precision + 1, '0',len_printf);
 		ft_putstr(&res[1], len_printf);
 		print_space((space - 1) - (zero_precision), ' ',len_printf);
-	}
-	else if(param->minus == 0 && param->zero == 1)
-	{
-		if (param->point > len - 1 && param->width < param->point)
-			zero_precision += 1;
-
-		len_printf->len += write(1,"-",1);
-		print_space(zero_precision + (space), '0',len_printf);
-		ft_putstr(&res[1], len_printf);
 	}
 	else if(param->minus == 0)
 	{
