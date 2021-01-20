958eb54ba022ad85f8b84b7a84c9ac30f653ec8c
Eric LING <eriling@e1r7p14.42.fr>
2021-01-20 14:56:32 +0100

    res_is_blank.c norme ok

--- print_diu_hex/res_is_blank.c
+++ print_diu_hex/res_is_blank.c
@@ -6,19 +6,19 @@
 /*   By: eriling <eriling@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/15 15:31:57 by eriling           #+#    #+#             */
-/*   Updated: 2021/01/15 15:44:52 by eriling          ###   ########.fr       */
+/*   Updated: 2021/01/20 14:55:35 by eriling          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/printf.h"
 
-void res_is_blank(int space, t_param_len *len_printf, t_param *param)
+void	res_is_blank(int space, t_param_len *len_printf, t_param *param)
 {
 	if (param->width > 0)
 	{
-		print_space(space + 1, ' ',len_printf);
-		return;
+		print_space(space + 1, ' ', len_printf);
+		return ;
 	}
-	print_space(space, ' ',len_printf);
-	return;
+	print_space(space, ' ', len_printf);
+	return ;
 }
