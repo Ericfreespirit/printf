fa557793ed600ee9cdfe2564496b7c2ed20a3a60
Eric LING <eriling@e1r7p14.42.fr>
2021-01-21 09:02:38 +0100

    printf-test and ftprintftdestructore done

--- print_c_percent/print_c_percent.c
+++ print_c_percent/print_c_percent.c
@@ -6,7 +6,7 @@
 /*   By: eriling <eriling@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/19 15:04:44 by eriling           #+#    #+#             */
-/*   Updated: 2021/01/20 14:39:47 by eriling          ###   ########.fr       */
+/*   Updated: 2021/01/21 08:59:53 by eriling          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,10 @@ void	print_c_percent(t_param *param, va_list arg, t_param_len *len_printf)
 	}
 	else if (param->minus == 0)
 	{
-		print_space(space, ' ', len_printf);
+		if (param->conv == '%' && param->zero == 1)
+			print_space(param->width - 1, '0', len_printf);
+		else
+			print_space(space, ' ', len_printf);
 		if (param->conv == 'c')
 			len_printf->len += write(1, &res, 1);
 		else if (param->conv == '%')
